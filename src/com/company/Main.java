b5315baeca88b378be1789632eb2351618c63e3a
Luka <luka.stojcev.2023@student.lhschools.org>
2020-03-12 12:59:06 -0400

    User Data Captured

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	
+        ModulusClock();
+    }
+
+    public static void ModulusClock() {
+
+        Scanner keyboard;
+        int hour;
+        int minutes;
+
+        keyboard = new Scanner(System.in);
+        System.out.println("Give me an hour in the day. 1-24");
+        hour = keyboard.nextInt();
+        System.out.println("Now give me any amount of minutes.");
+        minutes = keyboard.nextInt();
+        System.out.println(+hour);
+        System.out.println(+minutes);
     }
 }
